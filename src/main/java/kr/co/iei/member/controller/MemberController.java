f9316099e50ed0d76c0aad0769c0b859803ebac5
heesoo <goqkddnf@naver.com>
2024-08-30 19:35:40 +0900

    메인 페이지 만드는중~

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -2,11 +2,18 @@ package kr.co.iei.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.member.model.service.MemberService;
 
 @Controller
+@RequestMapping(value="/member")
 public class MemberController {
 	@Autowired
 	private MemberService memberServcie;
+	@GetMapping(value="/main")
+	public String main() {
+		return "member/main";
+	}
 }
